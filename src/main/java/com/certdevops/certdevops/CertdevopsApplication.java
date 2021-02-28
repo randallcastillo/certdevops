fb836ef4e1a2a618e015faa7e61d5fe3ee8a2711
randallcastillo <randallcastillo.dev@gmail.com>
2021-02-27 21:07:12 -0600

    Initial commit

---
+++ src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.certdevops.certdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertdevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certdevops/certdevops/CertdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certdevops.certdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
