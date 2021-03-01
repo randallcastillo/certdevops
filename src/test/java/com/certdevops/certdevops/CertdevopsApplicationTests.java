188458aa8d00e980d01349d680825cedc22418d5
randallcastillo <randallcastillo.dev@gmail.com>
2021-02-28 23:15:13 -0600

    Adding endpoints Testing

--- src/test/java/com/certdevops/certdevops/CertdevopsApplicationTests.java
+++ src/test/java/com/certdevops/certdevops/CertdevopsApplicationTests.java
@@ -6,15 +6,53 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = { "management.port=0" })
 class CertdevopsApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Value("${local.management.port}")
+	private int mgt;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Autowired
 	private HomeController controller;
 
 	@Test
-	public void contextLoads() throws Exception {
+	public void contextLoads_HomeController() throws Exception {
 		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void shouldReturn200WhenSendingRequestToController() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate
+				.getForEntity("http://localhost:" + this.port + "/hello-world", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	public void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate
+				.getForEntity("http://localhost:" + this.mgt + "/actuator/info", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
