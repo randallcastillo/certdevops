7910de368c18f8c56ec3e02f6d6803cb0e64fbc4
randallcastillo <randallcastillo.dev@gmail.com>
2021-02-28 20:00:34 -0600

    Spring app running on Maven

--- src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
+++ src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
@@ -3,7 +3,12 @@ package com.certdevops.certdevops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
 public class CertdevopsApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/certdevops/certdevops/CertdevopsApplicationTests.java
+++ src/test/java/com/certdevops/certdevops/CertdevopsApplicationTests.java
@@ -3,11 +3,18 @@ package com.certdevops.certdevops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootTest
 class CertdevopsApplicationTests {
 
+	@Autowired
+	private HomeController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
