f113d5244d2ff4c813f39f63b4b99b6e301b7132
randallcastillo <randallcastillo.dev@gmail.com>
2021-03-01 15:30:57 -0600

    Properties change

--- src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
+++ src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
---
+++ src/main/java/com/certdevops/certdevops/Product.java
@@ -0,0 +1,36 @@
+package com.certdevops.certdevops;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Model Class to map with the product table in the database
+ */
+@Entity
+public class Product {
+    private Integer id;
+    private String name;
+    private float price;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
