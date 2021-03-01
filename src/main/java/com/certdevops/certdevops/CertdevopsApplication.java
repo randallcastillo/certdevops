6c8b3fa1514ed91949287e95e176a6dab9d2138f
Randall Castillo <rocastillou@gmail.com>
2021-03-01 15:33:03 -0600

    Feature/readme (#2)
    * Adding README.md content
    
    * Update README.md Header
    
    * Jenkinsfile: dockerhub_cred
    
    * Properties change
    
    * Properties change

--- src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
+++ src/main/java/com/certdevops/certdevops/CertdevopsApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication // provides a load of defaults
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class CertdevopsApplication {
 
 	public static void main(String[] args) {
